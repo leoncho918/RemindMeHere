35e5a07acb7dae2c1a0a0cd231dd3334acc6230a
CHOsen One <37858778+leoncho918@users.noreply.github.com>
2018-09-18 01:00:38 +1000

    Added ID to reminder model

--- app/src/main/java/com/mad/remindmehere/model/Reminder.java
+++ app/src/main/java/com/mad/remindmehere/model/Reminder.java
@@ -3,16 +3,26 @@ package com.mad.remindmehere.model;
 import com.google.android.gms.maps.model.LatLng;
 
 public class Reminder {
+    private int mId;
     private String mName;
     private String mDescription;
     private LatLng mLatLng;
     private int mRadius;
 
-    public Reminder(String mName, String mDescription, LatLng mLatLng, int mRadius) {
+    public Reminder(int mId, String mName, String mDescription, LatLng mLatLng, int mRadius) {
+        this.mId = mId;
         this.mName = mName;
         this.mDescription = mDescription;
         this.mLatLng = mLatLng;
         this.mRadius = mRadius;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int mId) {
+        this.mId = mId;
     }
 
     public String getName() {
