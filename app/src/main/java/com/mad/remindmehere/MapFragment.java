cc513b4a17504f5d99e04c8d20d262d2ce83fa08
CHOsen One <37858778+leoncho918@users.noreply.github.com>
2018-09-21 02:44:48 +1000

    Map works in fragment

--- app/src/main/java/com/mad/remindmehere/MapFragment.java
+++ app/src/main/java/com/mad/remindmehere/MapFragment.java
@@ -10,11 +10,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MapFragment extends Fragment {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    private SupportMapFragment mapFragment;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_reminders_map, container, false);
+
+        if (mapFragment == null) {
+            mapFragment = SupportMapFragment.newInstance();
+            mapFragment.getMapAsync(this);
+        }
+
+        getChildFragmentManager().beginTransaction().replace(R.id.map, mapFragment).commit();
+
+        return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
