ea5cf1c66b7a9bd7dbdde41e1886e91a35cd4685
CHOsen One <37858778+leoncho918@users.noreply.github.com>
2018-10-11 21:48:54 +1100

    Adapters commented

--- app/src/main/java/com/mad/remindmehere/adapter/InfoWindowAdapter.java
+++ app/src/main/java/com/mad/remindmehere/adapter/InfoWindowAdapter.java
@@ -8,29 +8,52 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
 import com.mad.remindmehere.R;
 
+/**
+ * An adapter that provides a view for customised rendering of info windows
+ */
 public class InfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
+    //Activity variable to store the activity that creates an adapter object
     private Activity context;
 
+    /**
+     * Constructor that takes the calling activity's reference as a parameter
+     */
     public InfoWindowAdapter(Activity context) {
+        //Save activity
         this.context = context;
     }
 
+    /**
+     * Provides a custom info window for a marker
+     * @param marker
+     * @return
+     */
     @Override
     public View getInfoWindow(Marker marker) {
+        //Return null
         return null;
     }
 
+    /**
+     * Provides custom contents for the default info window frame of a marker
+     * @param marker
+     * @return
+     */
     @Override
     public View getInfoContents(Marker marker) {
+        //Create new view object to save custom layout view
         View view = context.getLayoutInflater().inflate(R.layout.infowindow, null);
 
+        //Store TextView ui widgets from custom layout view
         TextView nameTv = (TextView) view.findViewById(R.id.name_Tv);
         TextView addressTv = (TextView) view.findViewById(R.id.desc_Tv);
 
+        //Set textview text to marker title and snippet
         nameTv.setText(marker.getTitle());
         addressTv.setText(marker.getSnippet());
 
+        //Return custom layout view
         return view;
     }
 }
--- app/src/main/java/com/mad/remindmehere/adapter/ReminderAdapter.java
+++ app/src/main/java/com/mad/remindmehere/adapter/ReminderAdapter.java
@@ -27,70 +27,120 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * An adapter that provides binding for Reminders and sets it to a custom view which are displayed in a recyclerview
+ */
 public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.ViewHolder> {
 
+    //Variables to store data
     ArrayList<Reminder> mReminders;
     Context mContext;
     Activity mActivity;
+
+    //Constants
     public static final String LAT = "com.mad.remindmehere.ReminderAdapter.LAT";
     public static final String LNG = "com.mad.remindmehere.ReminderAdapter.LNG";
 
+    /**
+     * Constructor that takes a arraylist of reminders, context and the activity that called it as parameters
+     * @param context
+     * @param trains
+     * @param activity
+     */
     public ReminderAdapter(Context context, ArrayList<Reminder> trains, Activity activity) {
         this.mReminders = trains;
         this.mContext = context;
         this.mActivity = activity;
     }
 
+    /**
+     * Called when the recyclerview needs a new viewholder of reminder
+     * @param parent
+     * @param viewType
+     * @return
+     */
     @NonNull
     @Override
     public ReminderAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //Create new view to store custom layout for recyclerview
         View mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.reminder_item, parent, false);
 
+        //Create new viewholder to hold custom view
         ViewHolder mViewHolder = new ViewHolder(mView);
 
+        //Return viewholder
         return  mViewHolder;
     }
 
+    /**
+     * Called by the RecyclerView to display data to display data at the given position
+     * @param holder
+     * @param position
+     */
     @Override
     public void onBindViewHolder(@NonNull ReminderAdapter.ViewHolder holder, final int position) {
+        //Setting contents of itemview to display infor for each reminder object
         holder.mReminderName.setText(mReminders.get(position).getName());
         holder.mReminderAddress.setText(AddReminderActivity.getAddress(new LatLng(mReminders.get(position).getLat(), mReminders.get(position).getLng()), mContext));
         holder.mReminderRadius.setText(mContext.getResources().getString(R.string.reminder_item_radius) + mReminders.get(position).getRadius());
-
+        //Setting onclicklistener for each itemview
         holder.mReminderLayout.setOnClickListener(new View.OnClickListener() {
+            //Called when the itemview is clicked
             @Override
             public void onClick(View v) {
+                //Create new intent to send data back to previous activity
                 Intent resultIntent = new Intent();
+                //Store the reminder lat and lng coordinates for selected reminder item
                 double lat = mReminders.get(position).getLat();
                 double lng = mReminders.get(position).getLng();
+                //Put lat and lng coordinates into intent
                 resultIntent.putExtra(LAT, lat);
                 resultIntent.putExtra(LNG, lng);
+                //Set result code and intent
                 mActivity.setResult(RemindersMapsActivity.LIST_REMINDER, resultIntent);
+                //Close activity
                 mActivity.finish();
             }
         });
     }
 
+    /**
+     * Returns the total number of reminders in the data set being held by the adapter
+     * @return
+     */
     @Override
     public int getItemCount() {
         return mReminders.size();
     }
 
+    /**
+     * Deletes the reminder at the specified postion from the adapter's dataset
+     * @param position
+     */
     public void removeReminder(int position) {
         mReminders.remove(position);
         notifyItemRemoved(position);
         notifyItemRangeChanged(position, mReminders.size());
     }
 
+    /**
+     * Class to for recyclerview viewholder
+     */
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        //Variables to store ui widgets
         private TextView mReminderName;
         private TextView mReminderAddress;
         private TextView mReminderRadius;
         private RelativeLayout mReminderLayout;
 
+        /**
+         * Constructor for viewholder
+         * @param itemView
+         */
         public ViewHolder(View itemView) {
             super(itemView);
+            //Get view for each ui widget
             mReminderName = itemView.findViewById(R.id.reminder_name_tv);
             mReminderAddress = itemView.findViewById(R.id.reminder_address_tv);
             mReminderRadius = itemView.findViewById(R.id.reminder_radius_tv);
@@ -98,6 +148,10 @@ public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.ViewHo
         }
     }
 
+    /**
+     * Called by recyclerview when it starts observing the adapter
+     * @param recyclerView
+     */
     @Override
     public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
         super.onAttachedToRecyclerView(recyclerView);
